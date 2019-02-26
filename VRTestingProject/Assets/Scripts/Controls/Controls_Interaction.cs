abe7d3fa4d79d7cc2ef4292cc00c28290b8f2877
Kevin Otten <kevin.otten@movares.nl>
2019-02-26 14:41:41 +0100

    new project met werkende controll setup

---
+++ VRTestingProject/Assets/Scripts/Controls/Controls_Interaction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Controls_Interaction : MonoBehaviour
+{
+    public bool squeezedTrigger;
+    public float triggerValue;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ VRTestingProject/Assets/Scripts/Controls/ViveInputTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using Valve.VR;
+
+public class ViveInputTest : MonoBehaviour
+{
+    [Header("Actions")]
+    public SteamVR_Action_Single squeezeAction;
+    public SteamVR_Action_Vector2 touchPadAction;
+
+    [Header("Input Values")]
+    public bool touchPadTouch;
+    public bool touchPadClick;
+    public Vector2 touchpadValue;
+    public bool squeezedTrigger;
+    public float triggerValue;
+
+    void Update()
+    {
+        CheckInputs();
+    }
+
+    void CheckInputs()
+    {
+        triggerValue = squeezeAction.GetAxis(SteamVR_Input_Sources.Any);
+
+        touchpadValue = touchPadAction.GetAxis(SteamVR_Input_Sources.Any);
+
+        touchPadTouch = SteamVR_Actions.MyControllSet.TouchPad.state;
+
+        touchPadClick = SteamVR_Actions.MyControllSet.TouchPadClick.state;
+
+        squeezedTrigger = SteamVR_Actions.MyControllSet.TriggerDown.state;
+    }
+}
