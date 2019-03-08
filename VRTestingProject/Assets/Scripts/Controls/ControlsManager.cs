c6b23b83124f29e792eb34aa888ee50ade876e50
Kevin Otten <kevin.otten@movares.nl>
2019-03-08 17:04:35 +0100

    veranderingen aan type hand en bijna smooth movement

---
+++ VRTestingProject/Assets/Scripts/Controls/ControlsManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsManager : MonoBehaviour
+{
+    public static ControlsManager instance;
+
+
+    public Transform VR_Camera;
+    public Transform VR_CameraRig;
+    public float movementSpeed;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ VRTestingProject/Assets/Scripts/Controls/Controls_Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Controls_Movement : MonoBehaviour
+{
+    private SteamVR_Behaviour_Pose pose;
+
+    //Smooth\Teleportation movement values
+    public SteamVR_Action_Vector2 touchPadAction;
+    public bool touchPadTouch;
+    public Vector2 touchpadValue;
+    public bool touchPadClick;
+
+    void Start()
+    {
+        pose = GetComponent<SteamVR_Behaviour_Pose>();
+    }
+
+    void Update()
+    {
+        touchPadTouch = SteamVR_Actions.MyControllSet.TouchPad.state;
+        touchPadClick = SteamVR_Actions.MyControllSet.TouchPadClick.state;
+
+        if (touchPadTouch)
+        {
+            touchpadValue = touchPadAction.GetAxis(pose.inputSource);
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        //Get next position
+        Vector3 nextPosition = ControlsManager.instance.VR_Camera.right * (touchpadValue.x * ControlsManager.instance.movementSpeed) + ControlsManager.instance.VR_Camera.forward * (touchpadValue.y * ControlsManager.instance.movementSpeed) * Time.deltaTime;
+        nextPosition.y = 0;
+        nextPosition += ControlsManager.instance.VR_Camera.transform.position;
+
+        //Set position to next position
+        if (touchPadClick)
+        {
+            ControlsManager.instance.VR_CameraRig.position += nextPosition;
+        }
+    }
+}
